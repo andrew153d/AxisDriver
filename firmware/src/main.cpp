d79b44cc08e7df4ae6ec9ad34d0ef9188a3fa661
andrew <andrewwisnergrow@gmail.com>
2025-02-23 20:19:56 -0500

    led control with pwm

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,154 +1,145 @@
 #include <Arduino.h>
-#include "wiring_private.h" // pinPeripheral() function
-#include <AccelStepper.h>
-#include <TMCStepper.h>
-#include "Task/Task.h"
-#include "MessageProcessor/MessageProcessor.hpp"
-#include "LedController/LedController.h"
-#include "SerialTextInterface/SerialTextInterface.h"
-#include "MotorController/MotorController.h"
-#include "EncoderController/EncoderController.h"
-#include "DeviceManager/DeviceManager.h"
-#include "FlashStorage/FlashStorage.h"
-#include "AxisEthernet/AxisEthernet.h"
-#include "Messages.h"
-#include <cstdint>
-#include "Wire.h"
-#include "SPI.h"
-#include "DebugPrinter.h"
-
-class StatusLedDriver : public ITask
-{
-private:
-  uint32_t led_timer = 0;
-  bool led_state = 0;
-
-public:
-  StatusLedDriver(uint32_t period)
-  {
-    executionPeriod = period;
-  }
-
-  void OnStart() override
-  {
-    pinMode(STAT_LED, OUTPUT);
-  }
-
-  void OnStop() override
-  {
-  }
-
-  void OnRun() override
-  {
-    digitalWrite(STAT_LED, led_state = !led_state);
-  }
-};
-
-enum class HATType
-{
-  NONE,
-  ETHERNET,
-};
-
-void EvaluateHatType();
-
-TaskManager manager;
-MessageProcessor messageProcessor(0);
-// external interfaces
-SerialTextInterface serialTextInterface(0);
-
-// internal devices
-StatusLedDriver statusLight(1000);
-
-DeviceManager deviceManager(0xFFFFFFFF);
+#include "wiring_private.h"
 
 void setup()
 {
-  DEBUG_BEGIN(115200);
-  
-  Wire1.begin();
-  Wire1.setClock(400000);
+  // enable input, to support reading back values, with pullups disabled
+  PORT->Group[g_APinDescription[STAT_LED].ulPort].PINCFG[g_APinDescription[STAT_LED].ulPin].reg = (uint8_t)(PORT_PINCFG_INEN | PORT_PINCFG_DRVSTR);
 
-  pinMode(USR_INPUT, INPUT);
-  addrLedController.Start();
-  addrLedController.SetLEDColor(CRGB(0x001100));
-  uint32_t start_time = millis();
-  while(digitalRead(USR_INPUT) && (millis()-start_time)<1500){delay(10);}
-  if (!digitalRead(USR_INPUT))
-  {
-    while (!Serial)
-    {
-      addrLedController.Start();
-      digitalWrite(STAT_LED, LOW);
-      addrLedController.SetLEDColor(CRGB(0x0000F0));
-      delay(80);
-      digitalWrite(STAT_LED, HIGH);
-      addrLedController.SetLEDColor(CRGB(0x000000));
-      delay(80);
-    }
-  }
-
-  FlashStorage::Init();
-  addrLedController.SetLEDColor(CRGB(0x000000));
-
-  messageProcessor.AddExternalInterface(&serialTextInterface);
-
-  manager.AddTask(&serialTextInterface);
-  manager.AddTask(&messageProcessor);
-  manager.AddTask(&statusLight);
-  manager.AddTask(&addrLedController);
-  manager.AddTask(&motorController);
-  manager.AddTask(&encoderController);
-  //  start the interfaces
-  serialTextInterface.Start();
-
-  // start the message processor
-  messageProcessor.Start();
-  addrLedController.Start();
-  addrLedController.SetRainbowBrightness(100);
-
-  statusLight.Start();
-
-  encoderController.Start();
-  motorController.Start();
-  motorController.setEncoderValueSource(&encoderController);
-
-  EvaluateHatType();
+  // Set pin to output mode
+  PORT->Group[g_APinDescription[STAT_LED].ulPort].DIRSET.reg = 1 << g_APinDescription[STAT_LED].ulPin;
 }
 
-void EvaluateHatType()
+void myanalogWrite(uint32_t pin, uint32_t value)
 {
-  // check for the the ethernet hat
-  AxisEthernet *AEthernet = new AxisEthernet(10);
-  if (AEthernet != nullptr)
-    AEthernet->Start();
+  PinDescription pinDesc = g_APinDescription[pin];
+  uint32_t attr = pinDesc.ulPinAttribute;
 
-  if (AEthernet->IsPresent())
-  {
-    addrLedController.AddLedStep(CRGB::Purple, 100);
-    addrLedController.AddLedStep(CRGB::Black, 100);
-    manager.AddTask(AEthernet);
-  }
-  else
-  {
-    addrLedController.AddLedStep(CRGB::Red, 100);
-    addrLedController.AddLedStep(CRGB::Black, 100);
-    //delete AEthernet;
-    //AEthernet = nullptr;
-  }
+  
+
+    uint32_t tcNum = GetTCNumber(pinDesc.ulPWMChannel);
+    uint8_t tcChannel = GetTCChannelNumber(pinDesc.ulPWMChannel);
+    static bool tcEnabled[TCC_INST_NUM + TC_INST_NUM];
+
+    if (attr & PIN_ATTR_PWM_E)
+      pinPeripheral(pin, PIO_TIMER);
+    else if (attr & PIN_ATTR_PWM_F)
+      pinPeripheral(pin, PIO_TIMER_ALT);
+    else if (attr & PIN_ATTR_PWM_G)
+      pinPeripheral(pin, PIO_TCC_PDEC);
+
+    if (!tcEnabled[tcNum])
+    {
+      tcEnabled[tcNum] = true;
+      GCLK->PCHCTRL[GCLK_CLKCTRL_IDs[tcNum]].reg = GCLK_PCHCTRL_GEN_GCLK0_Val | (1 << GCLK_PCHCTRL_CHEN_Pos); // use clock generator 0
+
+      // Set PORT
+      if (tcNum >= TCC_INST_NUM)
+      {
+        // -- Configure TC
+        Tc *TCx = (Tc *)GetTC(pinDesc.ulPWMChannel);
+
+        // reset
+        TCx->COUNT8.CTRLA.bit.SWRST = 1;
+        while (TCx->COUNT8.SYNCBUSY.bit.SWRST)
+          ;
+
+        // Disable TCx
+        TCx->COUNT8.CTRLA.bit.ENABLE = 0;
+        while (TCx->COUNT8.SYNCBUSY.bit.ENABLE)
+          ;
+        // Set Timer counter Mode to 8 bits, normal PWM, prescaler 1/256
+        TCx->COUNT8.CTRLA.reg = TC_CTRLA_MODE_COUNT8 | TC_CTRLA_PRESCALER_DIV256;
+        TCx->COUNT8.WAVE.reg = TC_WAVE_WAVEGEN_NPWM;
+
+        while (TCx->COUNT8.SYNCBUSY.bit.CC0)
+          ;
+        // Set the initial value
+        TCx->COUNT8.CC[tcChannel].reg = (uint8_t)value;
+        while (TCx->COUNT8.SYNCBUSY.bit.CC0)
+          ;
+        // Set PER to maximum counter value (resolution : 0xFF)
+        TCx->COUNT8.PER.reg = 0xFF;
+        while (TCx->COUNT8.SYNCBUSY.bit.PER)
+          ;
+        // Enable TCx
+        TCx->COUNT8.CTRLA.bit.ENABLE = 1;
+        while (TCx->COUNT8.SYNCBUSY.bit.ENABLE)
+          ;
+      }
+      else
+      {
+        // -- Configure TCC
+        Tcc *TCCx = (Tcc *)GetTC(pinDesc.ulPWMChannel);
+
+        TCCx->CTRLA.bit.SWRST = 1;
+        while (TCCx->SYNCBUSY.bit.SWRST)
+          ;
+
+        // Disable TCCx
+        TCCx->CTRLA.bit.ENABLE = 0;
+        while (TCCx->SYNCBUSY.bit.ENABLE)
+          ;
+        // Set prescaler to 1/256
+        TCCx->CTRLA.reg = TCC_CTRLA_PRESCALER_DIV256 | TCC_CTRLA_PRESCSYNC_GCLK;
+
+        // Set TCx as normal PWM
+        TCCx->WAVE.reg = TCC_WAVE_WAVEGEN_NPWM;
+        while (TCCx->SYNCBUSY.bit.WAVE)
+          ;
+
+        while (TCCx->SYNCBUSY.bit.CC0 || TCCx->SYNCBUSY.bit.CC1)
+          ;
+        // Set the initial value
+        TCCx->CC[tcChannel].reg = (uint32_t)value;
+        while (TCCx->SYNCBUSY.bit.CC0 || TCCx->SYNCBUSY.bit.CC1)
+          ;
+        // Set PER to maximum counter value (resolution : 0xFF)
+        TCCx->PER.reg = 0xFF;
+        while (TCCx->SYNCBUSY.bit.PER)
+          ;
+        // Enable TCCx
+        TCCx->CTRLA.bit.ENABLE = 1;
+        while (TCCx->SYNCBUSY.bit.ENABLE)
+          ;
+      }
+    }
+    else
+    {
+      if (tcNum >= TCC_INST_NUM)
+      {
+        Tc *TCx = (Tc *)GetTC(pinDesc.ulPWMChannel);
+        TCx->COUNT8.CC[tcChannel].reg = (uint8_t)value;
+        while (TCx->COUNT8.SYNCBUSY.bit.CC0 || TCx->COUNT8.SYNCBUSY.bit.CC1)
+          ;
+      }
+      else
+      {
+        Tcc *TCCx = (Tcc *)GetTC(pinDesc.ulPWMChannel);
+        while (TCCx->SYNCBUSY.bit.CTRLB)
+          ;
+        while (TCCx->SYNCBUSY.bit.CC0 || TCCx->SYNCBUSY.bit.CC1)
+          ;
+        TCCx->CCBUF[tcChannel].reg = (uint32_t)value;
+        while (TCCx->SYNCBUSY.bit.CC0 || TCCx->SYNCBUSY.bit.CC1)
+          ;
+        TCCx->CTRLBCLR.bit.LUPD = 1;
+        while (TCCx->SYNCBUSY.bit.CTRLB)
+          ;
+      }
+    }
 }
-uint32_t loop_count = 0;
-uint32_t loop_count_timer = 0;
+
 void loop()
 {
-  loop_count++;
-  if(millis()-loop_count_timer>1000)
+  for (int brightness = 0; brightness <= 255; brightness++)
   {
-    loop_count_timer = millis();
-    //DEBUG_PRINTF("%d loops per second\n", loop_count/1);
-    //DEBUG_PRINTF("Position: %f\n", encoderController.GetPositionDegrees());
-    loop_count=0;
+    myanalogWrite(STAT_LED, brightness); // Increase brightness
+    delay(10);                         // Wait for 10 milliseconds
   }
-  manager.RunTasks();
-  FlashStorage::Task();
+  for (int brightness = 255; brightness >= 0; brightness--)
+  {
+    myanalogWrite(STAT_LED, brightness); // Decrease brightness
+    delay(10);                         // Wait for 10 milliseconds
+  }
 }
