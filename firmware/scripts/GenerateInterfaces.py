f1bddba007841231c8e1091e84145c2eb88cfc08
andrew <andrewwisnergrow@gmail.com>
2025-03-18 10:10:57 -0400

    python class generation complete

--- firmware/scripts/GenerateInterfaces.py
+++ firmware/scripts/GenerateInterfaces.py
@@ -17,33 +17,14 @@ public:
 };
 """
 
-
-# def generate_c_code(messages):
-#     c_code = """#include "messages.h"
-
-# """
-#     for msg in messages:
-#         if msg["type"] == "Custom":
-#             fields = msg["fields"]
-#             c_code += f"void Create{msg['name']}(uint8_t *buffer) {{\n"
-#             c_code += "    Header *header = (Header *)buffer;\n"
-#             c_code += f"    header->message_type = {msg['name']}_TYPE;\n"
-#             for field in fields:
-#                 c_code += f"    *(uint32_t *)(buffer + sizeof(Header)) = {field['name']};\n"
-#             c_code += "}\n\n"
-#         else:
-#             c_code += f"void Create{msg['name']}(uint8_t *buffer, uint32_t value) {{\n"
-#             c_code += "    Header *header = (Header *)buffer;\n"
-#             c_code += f"    header->message_type = {msg['name']}_TYPE;\n"
-#             c_code += "    *(uint32_t *)(buffer + sizeof(Header)) = value;\n"
-#             c_code += "}\n\n"
-#     return c_code
-
 def GetLen(type):
     print(type.split('[')[1])
     return 0
 
-def GetBytes(size):
+def GetBytes(size:str):
+    if(size.find("[")!=-1):
+        arr_len = int(size[size.find('[')+1:size.find(']')])
+        return arr_len * GetBytes(size[:size.find('[')])
     if size == "uint8_t":
         return 1
     if size == "int8_t":
@@ -61,71 +42,181 @@ def GetBytes(size):
     print(size)
     return 0
 
-def generate_python_code(messages):
-    py_code = """import struct\n\n"""
+def ConvertCTypeToPythonType(input_type):
+    if input_type == "uint8_t":
+        return "int"
+    if input_type == "int8_t":
+        return "int"
+    if input_type == "uint16_t":
+        return "int"
+    if input_type == "int16_t":
+        return "int"
+    if input_type == "uint32_t":
+        return "int"
+    if input_type == "int32_t":
+        return "int"
+    if input_type == "double":
+        return "float"
+    if input_type.find('[')!=-1:
+        return "bytearray"
+    return None
 
-    # make the predefined functions
-    for msg in messages["PredefinedMessages"]:
-        # function defenition
-        py_code += f"def Create{msg}("
-        first=True
-        for field in messages["PredefinedMessages"][msg]:
-            if(first):
-                first = False
-            else:
-                py_code+=", "
-            py_code += field['name']
-        py_code+='):\n'
-        py_code+="\tmessage = bytearray(0)\n"
-        
-        #fill in the fields
-        for field in messages["PredefinedMessages"][msg]:
-                py_code += (f"\tmessage += {field['name']}.to_bytes({GetBytes(field['type'])}, 'little')\n")
+def GenerateBaseClass(msg):
+    ret_string = ""
+    ret_string += f"class {msg['name']}: \n"
+    
+    #__init__
+    ret_string += f"\tdef __init__(self, "
+    ret_string += ", ".join([field['name'] for field in msg['fields']])
+    ret_string += "):\n"
+    for field in msg['fields']:
+        ret_string += f"\t\tself.{field['name']} = {field['name']}\n"
+    ret_string += "\n"
 
-        # return the result
-        py_code+="\treturn message\n\n"
+    # serialize
+    ret_string += "\tdef serialize(self):\n"
+    ret_string += "\t\tret = bytearray(0)\n"
+    for field in msg['fields']:
+        ret_string += f"\t\tret += self.{field['name']}.to_bytes({GetBytes(field['type'])}, 'little')\n"
+    ret_string += "\t\treturn ret\n\n"
 
-    already_defined_messages = []
-    for msg in messages["messages"]:
-        # function defenition
-        py_code += f"def {msg['name']}("
-        if msg['type'] == "Custom":
-            py_code += "message_id, "
-            first=True
-            for field in msg['fields']:
-                if(first):
-                    first = False
-                else:
-                    py_code+=", "
-                py_code += field['name']
-            py_code+='):\n'
-            py_code += "\tbody = bytearray(0)\n"
-            for field in msg['fields']:
-                if(field['type'].find('[')!=-1):
-                    py_code += f"\tbody+={field['name']}\n"
-                else:
-                    py_code += f"\tbody += {field['name']}.to_bytes({GetBytes(field['type'])},'little')\n"
+    #deserialize
+    ret_string += "\t@classmethod\n"
+    ret_string += "\tdef deserialize(cls, byte_array):\n"
+    counter = 0
+    for field in msg['fields']:
+        ret_string += f"\t\t{field['name']} = {ConvertCTypeToPythonType(field['type'])}.from_bytes(byte_array[{counter}:{GetBytes(field['type'])+counter}], 'little')\n"
+        counter += GetBytes(field['type'])
+    ret_string += "\t\treturn cls("
+    ret_string += ", ".join([field['name'] for field in msg['fields']])
+    ret_string += ")\n\n"
+
+    return ret_string
+
+def GeneratePredefinedClass(msg, predefined):
+    body_size = GetBytes(predefined[msg['type']]['fields'][0]['type'])
+    ret_string = ""
+    ret_string += f"class {msg['name']}: \n"
+    
+    #__init__
+    ret_string += f"\tdef __init__(self, msg_id, value):\n"
+    ret_string += f"\t\tself.body = {msg['type']}(value)\n"
+    ret_string += f"\t\tself.header = Header(msg_id, {body_size})\n"
+    ret_string += f"\t\tself.footer = Footer(sum(self.body.serialize())%0xFFFF)\n\n"
+
+    # serialize
+    ret_string+="\tdef serialize(self):\n"
+    ret_string+=    "\t\tret = bytearray(0)\n"
+    ret_string+=    "\t\tret += self.header.serialize()\n"
+    ret_string+=    "\t\tret += self.body.serialize()\n"
+    ret_string+=    "\t\tret += self.footer.serialize()\n"
+    ret_string+=    "\t\treturn ret\n\n"
+    
+    #deserialize
+    ret_string += "\t@classmethod\n"
+    ret_string += "\tdef deserialize(cls, byte_array):\n"
+    ret_string += "\t\theader = Header.deserialize(byte_array[0:4])\n"
+    ret_string += f"\t\tbody = {msg['type']}.deserialize(byte_array[4:{4+body_size}])\n"
+    ret_string += f"\t\tfooter = Footer.deserialize(byte_array[{4+body_size}:{4+body_size+2}])\n"
+    ret_string += f"\t\tmsg = cls(header.message_type, body.value)\n"
+    ret_string += f"\t\tmsg.body = body\n"
+    ret_string += f"\t\tmsg.header = header\n"
+    ret_string += f"\t\tmsg.footer = footer\n"
+    ret_string += "\t\treturn msg\n\n"
+    return ret_string
+
+def GenerateCustomClass(msg):
+    body_size = 0 #GetBytes(predefined[msg['type']]['fields'][0]['type'])
+    for field in msg['fields']:
+        body_size += GetBytes(field['type'])
+    
+    ret_string = ""
+    ret_string += f"class {msg['name'].split('Message')[0]}Body: \n"
+    #__init__
+    ret_string += f"\tdef __init__(self, "
+    ret_string += ", ".join([field['name'] for field in msg['fields']])
+    ret_string += "):\n"
+    for field in msg['fields']:
+        ret_string += f"\t\tself.{field['name']} = {field['name']}\n"
+    ret_string += "\n"
+
+    # serialize
+    ret_string += "\tdef serialize(self):\n"
+    ret_string += "\t\tret = bytearray(0)\n"
+    for field in msg['fields']:
+        if(field['type'].find('[')!=-1):
+            ret_string += f"\t\tret += self.{field['name']}\n"
         else:
-            py_code += "message_id, value):\n"
+            ret_string += f"\t\tret += self.{field['name']}.to_bytes({GetBytes(field['type'])}, 'little')\n"
+    ret_string += "\t\treturn ret\n\n"
 
-            #fill in the fields
-            py_code += f"\tbody = Create{msg['type']}(value)\n"
-        
-        py_code += f"\theader = CreateHeader(message_id, len(body))\n"
-        py_code += f"\tfooter = CreateFooter(sum(body) & 0xFF)\n"
-        # return the result
-        py_code+="\treturn header+body+footer\n\n"
+    #deserialize
+    ret_string += "\t@classmethod\n"
+    ret_string += "\tdef deserialize(cls, byte_array):\n"
+    counter = 0
+    for field in msg['fields']:
+        ret_string += f"\t\t{field['name']} = {ConvertCTypeToPythonType(field['type'])}.from_bytes(byte_array[{counter}:{GetBytes(field['type'])+counter}], 'little')\n"
+        counter += GetBytes(field['type'])
+    ret_string += f"\t\tmsg = cls("
+    ret_string += ", ".join([field['name'] for field in msg['fields']])
+    ret_string += ")\n"
+    ret_string += "\t\treturn msg\n\n"
+       
+    ret_string += f"class {msg['name']}: \n"
+    #__init__
+    ret_string += f"\tdef __init__(self, msg_id, "
+    ret_string += ", ".join([field['name'] for field in msg['fields']])
+    ret_string += "):\n"
+    ret_string += f"\t\tself.header = Header(msg_id, {body_size})\n"
+    ret_string += f"\t\tself.body = {msg['name'].split('Message')[0]}Body("
+    ret_string += ", ".join([field['name'] for field in msg['fields']])
+    ret_string += ")\n"
+    ret_string += f"\t\tself.footer = Footer(sum(self.body.serialize())%0xFFFF)\n\n"
 
-    py_code += "def print_bytes(byte_array):\n"
-    py_code += "\tprint(''.join(f'0x{byte:02x} ' for byte in byte_array))\n"
-    py_code += "print_bytes(CreateSetVelocityAndStepsMessage(0x11, 0x22, 0x33))\n"
-    return py_code
+    # serialize
+        # serialize
+    ret_string+="\tdef serialize(self):\n"
+    ret_string+=    "\t\tret = bytearray(0)\n"
+    ret_string+=    "\t\tret += self.header.serialize()\n"
+    ret_string+=    "\t\tret += self.body.serialize()\n"
+    ret_string+=    "\t\tret += self.footer.serialize()\n"
+    ret_string+=    "\t\treturn ret\n\n"
+
+    #deserialize
+    ret_string += "\t@classmethod\n"
+    ret_string += "\tdef deserialize(cls, byte_array):\n"
+    ret_string += "\t\theader = Header.deserialize(byte_array[0:4])\n"
+    ret_string += f"\t\tbody = {msg['name'].split('Message')[0]}Body.deserialize(byte_array[4:{4+body_size}])\n"
+    ret_string += f"\t\tfooter = Footer.deserialize(byte_array[{4+body_size}:{4+body_size+2}])\n"
+    ret_string += f"\t\tmsg = cls(header.message_type"
+    for field in msg['fields']:
+        ret_string += f", body.{field['name']}"
+    ret_string += ")\n"
+    ret_string += f"\t\tmsg.body = body\n"
+    ret_string += f"\t\tmsg.header = header\n"
+    ret_string += f"\t\tmsg.footer = footer\n"
+    ret_string += "\t\treturn msg\n\n"
+
+    return ret_string
 
 with open("interface.json", "r") as file:
     messages = json.load(file)
 
-# with open("messages.c", "w") as file:
-#     file.write(generate_c_code(messages))
-
 with open("examples/Python/automessages.py", "w") as file:
-    file.write(generate_python_code(messages))
+    
+    #Create Message Defenitions
+    id = 0x4F # non zero start
+    for m in messages["MessageIds"]:
+        file.write(f"{m} = 0x{id:X}\n")
+        id = id+1
+    file.write("\n\n")
+    predefined_messages = {}
+    for msg in messages["Messages"]:
+        if(msg['type'] == 'Base'):
+            file.write(GenerateBaseClass(msg))
+            predefined_messages[msg['name']] = msg
+        elif(msg['type'] == 'Custom'):
+            file.write(GenerateCustomClass(msg))
+            pass
+        else:
+            file.write(GeneratePredefinedClass(msg, predefined_messages))
